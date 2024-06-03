155017a5b2d0579c201a5cf44de54bfb98b67f63
l1qwie <programjibogdan@gmail.com>
2024-06-03 19:37:03 +0300

    ?

--- postgreSQL/set.sql
+++ postgreSQL/set.sql
@@ -10,6 +10,6 @@ INSERT INTO Schedule (game_id, sport, date, time, seats, latitude, longitude, ad
 
 INSERT INTO Schedule (game_id, sport, date, time, seats, latitude, longitude, address, price, currency, status) VALUES 
     (88888, 'football', 20241222, 1700, 46, 36.893445, 30.709591, 'Al, da?', 19, 'UT', -1);
-INSERT INTO Admins_password (password) VALUES ('1111');
+INSERT INTO Admins_password (password) VALUES ('111');
 INSERT INTO Users (user_id, username, name, last_name, level, action, language, user_admin) VALUES (-1, 'l1qwie', 'nataniel', 'sahar', 3, 'divarication', 'ru', true);
 INSERT INTO WatingForGamesUsers (user_id, game_id, seats, payment) VALUES ( -1, 9122, 6, 'card');
